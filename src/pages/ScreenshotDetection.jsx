2904bcffcf0e214ede02a73381e2f72673306dc7
jsong73 <jessicajinsong@gmail.com>
2024-02-01 22:47:23 -0500

    zoom image upload done

--- src/pages/ScreenshotDetection.jsx
+++ src/pages/ScreenshotDetection.jsx
@@ -4,6 +4,7 @@ import { extractNames, detectImage } from "../utils/tesseract";
 function ScreenshotDetection() {
 
   const [rosterLastNames, setRosterLastNames] = useState([]);
+  const [zoomLastNames, setZoomLastNames] = useState([]);
 
   const rosterImageUpload = async(e) =>{
     const rosterListImage = e.target.files[0]
@@ -22,13 +23,41 @@ function ScreenshotDetection() {
       console.log("error:", error)
     }
   }
-  }
+}
+
+const zoomImageUpload = async(e) => {
+    const zoomImageUpload = e.target.files;
+    console.log(zoomImageUpload)
+
+    if(zoomImageUpload.length > 0) {
+        try{
+            const lastNames = [];
+            for (const zoomImage of zoomImageUpload) {
+                const detectedNames = await detectImage(zoomImage);
+                // excludes instructors and Ts
+                const excludedNames = ["Jessica Song(TA)","Junghoon Yoon (Host)"];
+                const filteredNames = extractNames(detectedNames)
+                .filter(name => !excludedNames.includes(name));
+                lastNames.push(...filteredNames);
+
+                console.log(filteredNames)
+            }
+            setZoomLastNames(lastNames);
+        } catch(error){
+            console.log("error:", error)
+        }
+    }
+
+}
 
   return (
     <div>
             <h1>Attendance Checker</h1>
-            <label> Upload your attendance list </label>
+            <label> Upload your attendance roster list </label>
             <input type="file" id="rosterImageUpload" onChange={rosterImageUpload} />
+
+            <label> Upload Zoom participants list </label>
+            <input type="file" id="zoomImageUpload" onChange={zoomImageUpload} />
 
     </div>
   )
