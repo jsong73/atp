6a6f6a242eb515fbbfe28e51698e1538b4c18351
jsong73 <jessicajinsong@gmail.com>
2024-02-05 18:10:43 -0500

    icons for detection

--- client/src/pages/IntroPage.jsx
+++ client/src/pages/IntroPage.jsx
@@ -1,15 +1,17 @@
 import React from "react"
 import { Link } from 'react-router-dom';
+import ScreenshotMonitorIcon from '@mui/icons-material/ScreenshotMonitor';
+import CameraIcon from '@mui/icons-material/Camera';
 
 function IntroPage() {
     return (
       <div>
         <h1>Choose Detection Type</h1>
         <Link to="/screenshot-detection">
-          <button>Click here for Screenshot Detection</button>
+          <button className="detectionBtn"> Screenshot Detection <CameraIcon style={{ verticalAlign: "middle", marginBottom: "2px", marginLeft: "5px" }}/> </button>
         </Link>
         <Link to="/video-detection">
-          <button>Click here for Video Detection</button>
+          <button className="detectionBtn">Video Detection <ScreenshotMonitorIcon style={{ verticalAlign: "middle", marginBottom: "2px", marginLeft: "5px" }}/> </button>
         </Link>
       </div>
     );
